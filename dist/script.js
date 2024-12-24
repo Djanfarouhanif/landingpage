a4444789f255b6b6ddc45e66192652d163074bd8
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-12-24 11:20:22 +0000

    add par for prospect

---
+++ dist/script.js
@@ -0,0 +1,6 @@
+const hanifContainer = document.querySelector(".hanif");
+const hanif = document.getElementById("hanif");
+
+hanif.addEventListener("click", () => {
+  hanifContainer.classList.add("hanif-toggle");
+});
